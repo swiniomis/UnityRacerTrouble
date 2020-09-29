128611f56a0a1f675c0f7c80f2a8f8ba5f9bfc1f
Michał <michal_k@hotmail.com>
2020-09-30 00:32:15 +0200

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(speed,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
